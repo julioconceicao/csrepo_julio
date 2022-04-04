a5070587915c2c56ac611e4f8c1328a0c2565938
julioconceicao <julioconceicao.gtr@gmail.com>
2022-04-04 20:14:56 -0300

    @julioconceicao commit-04.04.22

---
+++ CalculoIMC(console).cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IMC
+{
+    internal class IMClesson
+    {
+        static void Main(string[] args)
+        {
+            float altura, peso, imc;
+
+            Console.WriteLine("Qual o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Olá" + nome + ". Agora me diga sua altura por favor: ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Perfeito. Agora me diga seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + imc);
+        
+        }
+    }
+}
---
+++ Lista1_CS/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ProjetoHelloWorld
+
+{
+    internal class ProjetoHelloWorld 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Beautiful World");
+            
+        }
+    }    
+}
