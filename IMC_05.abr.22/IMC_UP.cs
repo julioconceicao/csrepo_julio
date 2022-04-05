72cd018f421c6b13bb5706c9c15098808a4c53a3
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-04-05 15:36:25 -0300

    @julioconceicao 05.abr.22

---
+++ IMC_05.abr.22/IMC_UP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ProjVSC0504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float altura, peso, imc;
+             
+            Console.WriteLine("Roi, vem sempre aqui? Qual seu nome chuchu?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Olá " + nome + "!" + "Vamos calcular seu IMC! Se importa em me dizer: qual o seu peso? ");
+            peso = float.Parse (Console.ReadLine());
+
+            Console.WriteLine("Ok, e qual a sua altura em metros?");
+            altura = float.Parse (Console.ReadLine());
+
+           
+            imc = peso / (altura * altura);
+            if(imc < 18.5)
+            {
+                Console.WriteLine("Seu imc é: " + imc + ". " + "Abaixo do peso recomendado. ");
+            }
+            else if(imc <25)
+            {
+                Console.WriteLine("Seu imc é: " + imc + ". " + "Peso normal. ");
+            }       
+            else if(imc < 30)
+            {
+                Console.WriteLine("Seu imc é: " + imc + "." + "Sobrepeso. Projetinho fellas. ");
+            }
+            else if (imc >= 30)
+            {
+                Console.WriteLine("Seu imc é: " + imc + ". " + "Obesidade. O importante é se amar <3");
+            }
+
+        }
+    }
+}
