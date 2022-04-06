946ec96dec755a29ccb8dc20dabf74e47061899f
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-04-06 15:15:30 -0300

    @julioconceicao Aula.06.04.22

---
+++ Aula.06.04.2022/SOMA/SOMA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SOMA060422
+{
+    class SOMAEX1
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            for(int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"Insira o {i}° valor que deseja somar: ");
+                soma += int.Parse(Console.ReadLine());
+            }
+            //Equivalente à função print do python.
+            Console.WriteLine($"Valor da soma: {soma}");
+        }
+    }
+}
---
+++ Aula.06.04.2022/TABUADA/TABUADA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TABUADA
+{
+    class TABUADA
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuadinha <3");
+            Console.WriteLine("Quer saber a tabuada de qual número? ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= 10; x++)
+            {
+                int resultado = (numero * x);
+                Console.WriteLine($"{numero} * {x}  = {resultado}");
+            }      
+        }    
+    }
+}
